4965b619978050e3e1318411b7f6af9843967b36
xixiaofinland <xixiaofinland@users.noreply.github.com>
2025-03-25 18:55:51 +0000

    deploy: b61393288341a31fd2a0ea657f0bdb66e40d221f

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="intro.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="trait.html"><strong aria-hidden="true">2.</strong> Trait</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="common-traits.html"><strong aria-hidden="true">2.1.</strong> Common traits</a></li></ol></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="intro.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="trait.html"><strong aria-hidden="true">2.</strong> Trait</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="trait-common-traits.html"><strong aria-hidden="true">2.1.</strong> Common traits</a></li></ol></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
